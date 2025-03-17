5780165839bb6d5b643234841145d3ec801e735f
Justin Jiang <jus3472@gmail.com>
2025-03-16 19:53:01 -0500

    Initial Commit

---
+++ ARFace/ARFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFaceApp.swift
+//  ARFace
+//
+//  Created by Justin Jiang on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ARFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
